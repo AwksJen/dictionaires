7ef468eb9bd9a4bbd8def4f0ac5e7606ad068662
Your Name <you@example.com>
2020-02-03 09:26:23 -0800

    add one python file and two text files

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+# put your code here.
+#import test.txt
+#import twain.txt
+
+
+words = {}
+
+def word_count(file):
+    file = open(file).read()
+    file = file.split()
+    for word in file:
+        words[word] = words.get(word, 0) + 1
+
+    return words
+
+print(word_count('test.txt'))
